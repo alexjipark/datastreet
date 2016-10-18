7caf9f3a0b2d02c3a82902b32abbf30e795c566f
alexjipark <alexjipark@gmail.com>
2016-10-18 10:28:19 +0900

    Dummy integrated

--- app/app.go
+++ app/app.go
@@ -1,1 +1,53 @@
-package app
+package datastreet
+
+import (
+	"github.com/tendermint/go-merkle"
+	"github.com/tendermint/tmsp/types"
+	. "github.com/tendermint/go-common"
+	"strings"
+)
+
+type DataStreetApp struct {
+	state merkle.Tree
+}
+
+
+func NewDataStreetApp() *DataStreetApp {
+	state := merkle.NewIAVLTree(0, nil, )
+	return &DataStreetApp{state:state}
+}
+
+func (app *DataStreetApp) Info() string {
+	return Fmt("DataStreet size:%v", app.state.Size())
+}
+
+
+func (app *DataStreetApp) SetOption(key string, value string) (log string) {
+	return ""
+}
+
+// tx is either "key=value" or just arbitrary bytes
+func (app *DataStreetApp) AppendTx(tx []byte) types.Result {
+	parts := strings.Split(string(tx),"=")
+	if len(parts) == 2 {
+		app.state.Set([]byte(parts[0]), []byte(parts[1]))
+	} else {
+		app.state.Set (tx, tx)
+	}
+	return types.OK
+}
+
+func (app *DataStreetApp) CheckTx(tx []byte) types.Result {
+	return types.OK
+}
+
+func (app *DataStreetApp) Commit() types.Result {
+	hash := app.state.Hash()
+	return types.NewResultOK(hash,"")
+}
+
+func (app *DataStreetApp) Query(query []byte) types.Result {
+	index, value, exists := app.state.Get(query)
+	resStr := Fmt("Index=%v value=%v exists=%v", index, string(value), exists)
+	return types.NewResultOK([]byte(resStr), "")
+}
--- cmd/datastreet/main.go
+++ cmd/datastreet/main.go
@@ -1,1 +1,25 @@
-package datastreet
+package main
+
+import (
+	"flag"
+	"github.com/tendermint/tmsp/server"
+	"github.com/tendermint/go-common"
+	"github.com/alexjipark/datastreet/app"
+)
+
+func main() {
+	addrPtr := flag.String("addr", "tcp://0.0.0.0:46658", "Listen Address")
+	tmspPtr := flag.String("tmsp", "socket", "socket | grpc")
+	flag.Parse()
+
+	// Start the Listener
+	_, err := server.NewServer(*addrPtr, *tmspPtr, datastreet.NewDataStreetApp())
+	if err != nil {
+		//os.Exit(err.Error())
+	}
+
+	// Wait Forever
+	common.TrapSignal(func() {
+
+	})
+}
